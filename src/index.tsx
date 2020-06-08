88725c1dd7f8dfe1953fd204ee8a29770203f237
Jonas Obrist <jonas.obrist@hennge.com>
2020-06-08 18:09:24 +0900

    v3 t macro example

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {I18nProvider} from "@lingui/react";
+import {i18n} from "@lingui/core";
+import {t, Trans} from "@lingui/macro";
+
+i18n.loadLocaleData("en", {});
+i18n.loadLocaleData("ja", {});
+
+export async function activate(locale: string) {
+  const { messages } = await import(
+    /* webpackChunkName: "i18n-[index]" */ `./locales/${locale}/messages.js`
+  );
+  i18n.load(locale, messages);
+  i18n.activate(locale);
+}
+
+activate("en");
+
+const Message = () => {
+  return <><h1>{t`msgid`}</h1><h2><Trans>msgid</Trans></h2></>;
+}
+
+const Switcher = () => {
+  const [locale, setLocale] = useState("en");
+  return <select value={locale} onChange={e => {
+    activate(e.target.value);
+    setLocale(e.target.value);
+  }}><option value="en">English</option><option value="ja">日本語</option></select>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nProvider i18n={i18n}>
+      <Message />
+      <Switcher />
+    </I18nProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/locales/ja/messages.js
@@ -0,0 +1,1 @@
+/*eslint-disable*/module.exports={messages:{"msgid":"\u3053\u3093\u306B\u3061\u306F\uFF01"}};
